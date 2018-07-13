1e98d83e522b58af97b92eb980c1ba9b282602e7
auto-joe <jgrabenstein@mail.csuchico.edu>
2018-07-13 01:45:35 +0000

    Add timeout to scan, add config.json

--- app.py
+++ app.py
@@ -7,13 +7,45 @@ import os
 import subprocess
 import requests
 import logging
+import os
+import socket
+
 
 #Logging configuration
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
 
+#Get config.json values
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+
 app = Flask(__name__)
 api = Api(app)
-port = "2323"
+workerPort = config['worker_port']
+broadcastPort = config['broadcast_port']
+debugging = config['debug']
+
+def get_lan_ip():
+    ip = socket.gethostbyname(socket.gethostname())
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = [
+            "eth0",
+            "eth1",
+            "eth2",
+            "wlan0",
+            "wlan1",
+            "wifi0",
+            "ath0",
+            "ath1",
+            "ppp0",
+            ]
+        for ifname in interfaces:
+            try:
+                ip = get_interface_ip(ifname)
+                break
+            except IOError:
+                pass
+    return ip
 
 
 def find_ip(str):
@@ -31,8 +63,11 @@ def find_ip(str):
 
 
 
-def scan_network(port):
-    response="nmap -sP 192.168.1.2/24"
+def scan_network(port, ip):
+    ip=ip.rsplit(".", 1)[0]
+    ip=ip+".0"
+    response="nmap -sP "+ip+"/24"
+    #response="nmap -sP 192.168.1.2/24"
     response=response.split(" ")
     response=subprocess.check_output(response)
     #response=str(response).split('\n')
@@ -47,14 +82,15 @@ def scan_network(port):
 
     for x in ipList:
         try:
-            stats=requests.get("http://"+x+":"+port)
+            stats=requests.get("http://"+x+":"+workerPort, timeout=.01)
             #pprint(stats.json())
             worker_id=stats.json()['worker_id']
             hashrates=stats.json()['hashrate']['total']
-            #print("found worker "+worker_id+" at "+x+":"+port)
-            logging.info("Found worker "+worker_id+" at "+x+":"+port)
+            #print("found worker "+worker_id+" at "+x+":"+workerPort)
+            logging.info("Found worker "+worker_id+" at "+x+":"+workerPort)
             workerList.append({"name":worker_id,"ip":x, "hashrate":{"10s":hashrates[0],"60s":hashrates[1],"15m":hashrates[2]}})
         except:
+            logging.warning("No worker at "+x+":"+workerPort)
             pass
     return workerList
 
@@ -75,8 +111,8 @@ api.add_resource(Cluster, "/cluster")
 class Refresh(Resource):
     def post(self):
         global workers
-        logging.info("Rescanning network for workers on port "+port)
-        workers = scan_network(port)
+        logging.info("Rescanning network for workers on port "+workerPort)
+        workers = scan_network(workerPort, ip)
         logging.info("Stats updated for all workers ("+str(len(workers))+")")
         return workers
 
@@ -86,18 +122,28 @@ class Machine(Resource):
     def get(self, name):
         for worker in workers:
             if(name == worker["name"]):
-                stats=requests.get("http://"+worker['ip']+":"+port)
+                stats=requests.get("http://"+worker['ip']+":"+workerPort)
                 hashrates=stats.json()['hashrate']['total']
-                #print("updated stats for worker "+name+" at "+worker['ip']+":"+port)
-                logging.info("Updated stats for worker "+name+" at "+worker['ip']+":"+port)
+                #print("updated stats for worker "+name+" at "+worker['ip']+":"+workerPort)
+                logging.info("Updated stats for worker "+name+" at "+worker['ip']+":"+workerPort)
                 worker['hashrate'] = {"10s":hashrates[0],"60s":hashrates[1],"15m":hashrates[2]}
                 return worker, 200
         return "machine not found", 404
 
 api.add_resource(Machine, "/machine/<string:name>")
 
+if os.name != "nt":
+    import fcntl
+    import struct
 
-workers=scan_network(port)
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
+ip = get_lan_ip()
+logging.info("Localhost IP is: "+str(ip))
+workers=scan_network(workerPort, ip)
+logging.info("Searched on port: "+workerPort)
 logging.info("Initial network scan complete.")
 logging.info("Found "+str(len(workers))+" workers.")
-app.run(debug=True)
+app.run(debug=debugging, port=int(broadcastPort))
